e0a7105427569deb69296ba832075b8c9c71b19e
TataBrn <96158820+TataBrn@users.noreply.github.com>
2023-05-15 04:32:41 +0300

    Unit tests

--- src/test/java/CalculatorTests.java
+++ src/test/java/CalculatorTests.java
@@ -6,40 +6,34 @@ public class CalculatorTests {
 
 
     @Test
-    public void sumDoubleAndDouble(){
+    public void summaWorks(){
         Calculator culc = new Calculator(2.2, 3.42, '+');
         Assert.assertEquals("Сумма двух чисел не работет",5.62, culc.doOperation(), 0.0 );
     }
 
 
     @Test
-    public void difDoubleAndDouble(){
+    public void differenceWorks(){
         Calculator culc = new Calculator(2.2, 3.42, '-');
         Assert.assertEquals("Частное двух чисел не работет",-1.22, culc.doOperation(), 0.1);
     }
 
 
     @Test
-    public void multiplNegativeAndPositive(){
-        Calculator culc = new Calculator(2.2, -1, '*');
-        Assert.assertEquals("Умножение положительного и отрицательного чисел не работет",-2.2, culc.doOperation(), 0);
+    public void multiplicationWorks(){
+        Calculator culc = new Calculator(2.2, 2, '*');
+        Assert.assertEquals("Умножение не работет",4.4, culc.doOperation(), 0);
     }
 
     @Test
-    public void divisionDoubleAndDouble(){
-        Calculator culc = new Calculator(2.2, 1, '/');
-        Assert.assertEquals("Деление чисел чисел не работет",2.2, culc.doOperation(), 0);
+    public void divisionWorks(){
+        Calculator culc = new Calculator(4.4, 2, '/');
+        Assert.assertEquals("Деление чисел не работет",2.2, culc.doOperation(), 0);
     }
 
 
     @Test
-    public void sumDoubleAndZeroShouldBeDouble(){
-        Calculator culc = new Calculator(2.2, 0, '+');
-        Assert.assertEquals("Сумма любого числа и нуля должна равняться числу",2.2, culc.doOperation(), 0 );
-    }
-
-    @Test
-    public void devDoubleAndZeroShouldBeException(){
+    public void devisionNumberAndZeroShouldBeException(){
         Calculator culc = new Calculator(2.2, 0, '/');
         IllegalArgumentException thrown = Assert.assertThrows("При делении на ноль должно быть исключение",
                 IllegalArgumentException.class, culc::doOperation);
